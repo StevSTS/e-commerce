efa5bddc3313fc5495aacb48b3bb7f9257f8b64f
Steven Sary <150943045+StevSTS@users.noreply.github.com>
2024-07-08 20:50:00 +0300

    .

--- src/Components/userRate/UserStars.tsx
+++ src/Components/userRate/UserStars.tsx
@@ -5,15 +5,14 @@ const UserStars = () => {
 
     
     
-    const ra = useRef<HTMLParagraphElement>(null)
+    const ra = useRef<any>(null)
     useEffect(() => {
         let stars = document.querySelectorAll('.ratings .span')
-        stars.forEach(star => {
+        stars.forEach((star : any) => {
             star.addEventListener('click' , () => {
                 stars.forEach((star) => {
                     star.removeAttribute('checked')
                 })
-
                 let rating = star.dataset.rating;
                 star.setAttribute('checked' , 'true')
                 ra.current.innerHTML = rating + ' out of 5'
@@ -27,19 +26,6 @@ const UserStars = () => {
     <div>
         <p className="text-center mb-2 " ref={ra}>0 out of 5</p>
         <div className='ratings flex items-center justify-center text-[30px] cursor-pointer flex-row-reverse'>
-
-            {/* <input name='rating' id='star5' type="radio" value={5} /> */}
-            {/* <input name='rating' id='star4' type="radio" value={4} /> */}
-            {/* <input name='rating' id='star3' type="radio" value={3} /> */}
-            {/* <input name='rating' id='star2' type="radio" value={2} /> */}
-            {/* <input name='rating' id='star1' type="radio" value={1} /> */}
-            {/* <input name='rating' id='star2.5' type="radio" value={2.5} /><label htmlFor="star2.5" className='half'></label> */}
-            {/* <input name='rating' id='star1.5' type="radio" value={1.5} /><label htmlFor="star1.5" className='half'></label> */}
-            {/* <input name='rating' id='star4.5' type="radio" value={4.5} /><label htmlFor="star4.5" className='half'></label> */}
-            {/* <input name='rating' id='star3.5' type="radio" value={3.5} /><label htmlFor="star3.5" className='half'></label> */}
-            {/* <input name='rating' id='star0.5' type="radio" value={0.5} /><label htmlFor="star0.5" className='half'></label> */}
-
-
             <span data-rating="5" className="span py-1 px-2 cursor-pointer " ><AiFillStar /></span>
             <span data-rating="4" className="span py-1 px-2 cursor-pointer " ><AiFillStar /></span>
             <span data-rating="3" className="span py-1 px-2 cursor-pointer " ><AiFillStar /></span>
